f509dacd038ed21943926725bb0a7787cb945981
mjkonkel <mitchell.konkel@gmail.com>
2023-01-11 22:06:55 -0600

    started creating models

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    // format timestamp
+  },
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
